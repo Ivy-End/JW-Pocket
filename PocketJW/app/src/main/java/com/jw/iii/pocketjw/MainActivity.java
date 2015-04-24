e7acb43a9d761ec7a91cab6c90929b567af742b5
Ivy-End <admin@ivy-end.com>
2015-04-24 22:08:17 +0800

    sdk build in

--- PocketJW/app/src/main/java/com/jw/iii/pocketjw/MainActivity.java
+++ PocketJW/app/src/main/java/com/jw/iii/pocketjw/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.avos.avoscloud.*;    // 导入LeanCloud文件
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +13,10 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 以下两行连接LeanCloud，切勿修改
+        AVOSCloud.initialize(this, "dohthw768v153y9t2eldqiwvtwf9vu07vvyzxv4kjdqbpdsf", "gs5r4j0xg7wg0xkmvjrgdv4gt1hxiaqxpso3jfzani5w8hhk");
+        AVAnalytics.trackAppOpened(getIntent());
     }
 
 
